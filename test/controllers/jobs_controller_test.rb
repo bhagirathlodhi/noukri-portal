023c9f7bc1ee315fc56ad66b20690636e04224b7
bhagirathlodhi <bhagi.lodhi8989@gmail.com>
2023-09-26 17:33:46 +0530

    Inisilize repo

---
+++ test/controllers/jobs_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class JobsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get jobs_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get jobs_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get jobs_edit_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get jobs_destroy_url
+    assert_response :success
+  end
+end
